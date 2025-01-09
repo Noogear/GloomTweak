9eaafc7010b3b19cd755a3cf3651eabff4bbb653
Noogear <2364947161@qq.com>
2025-01-10 03:47:51 +0800

    Update EggToFeather.java

--- src/main/java/cn/gloomTweak/modules/mob/EggCapture/EggToFeather.java
+++ src/main/java/cn/gloomTweak/modules/mob/EggCapture/EggToFeather.java
@@ -2,6 +2,7 @@ package cn.gloomTweak.modules.mob.EggCapture;
 
 import cn.gloomTweak.Configuration;
 import org.bukkit.Material;
+import org.bukkit.entity.Chicken;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,6 +16,7 @@ public class EggToFeather implements Listener {
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
     public void onEggDrop(EntityDropItemEvent event) {
+        if (!(event.getEntity() instanceof Chicken)) return;
         Item drop = event.getItemDrop();
         if (drop.getItemStack().getType() == Material.EGG) {
             if (ThreadLocalRandom.current().nextInt(100) < Configuration.Mob.EggCapture.balance.eggToFeather) {
